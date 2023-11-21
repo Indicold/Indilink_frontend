33988a3620c25863dd97bfefb4a1314fcb296703
ravindra.kumar <ravindra.kumar@vector3.digital>
2023-11-21 16:40:38 +0530

    Notification issue fixed

--- src/components/template/NotificationDropdown/NotificationDropdown.tsx
+++ src/components/template/NotificationDropdown/NotificationDropdown.tsx
@@ -40,11 +40,11 @@ const dropdownItemList: DropdownList[] = []
 const _NotificationDropdown = ({ className }: CommonProps) => {
     const { token }: any = getToken();
     const { data: ListOfNotification, loading: Notificationloading, error: PCerror } =
-    useApiFetch<any>(`master/notifications`, token);
+        useApiFetch<any>(`master/notifications`, token);
 
     const { signOut } = useAuth()
-    
-const navigate:any=useNavigate();
+
+    const navigate: any = useNavigate();
 
     /* The `UserAvatar` constant is a JSX element that represents the avatar or icon for the user in
     the notification dropdown. It consists of a `Stack` component from the Material-UI library,
@@ -82,14 +82,14 @@ const navigate:any=useNavigate();
         };
     }
 
-// Example usage:
-const targetDate = "2023-09-15T05:11:27.000Z";
-const timeDifference = calculateTimeDifference(targetDate);
+    // Example usage:
+    const targetDate = "2023-09-15T05:11:27.000Z";
+    const timeDifference = calculateTimeDifference(targetDate);
 
-console.log("Time Difference:");
-console.log(`${timeDifference.days} days`);
-console.log(`${timeDifference.hours} hours`);
-console.log(`${timeDifference.minutes} minutes`);
+    console.log("Time Difference:");
+    console.log(`${timeDifference.days} days`);
+    console.log(`${timeDifference.hours} hours`);
+    console.log(`${timeDifference.minutes} minutes`);
 
     return (
         <div>
@@ -99,33 +99,33 @@ console.log(`${timeDifference.minutes} minutes`);
                 renderTitle={UserAvatar}
                 placement="bottom-end"
             >
-                    <Dropdown.Item variant="header" className='mt-2 px-6 font-bold text-base text-gray-800'>
-                        Notification
+                <Dropdown.Item variant="header" className='mt-2 px-6 font-bold text-base text-gray-800'>
+                    Notification
+                </Dropdown.Item>
+                <Dropdown.Item variant="divider" />
+                {ListOfNotification?.data?.slice(0, 4).map((item: any, index: any) => (
+                    <Dropdown.Item variant="header">
+                        <div className="flex items-center">
+                            <div className="mt-2 px-6  bg-white rounded-lg w-full">
+                                <div className=" inline-flex items-center justify-between w-full">
+                                    <div className="inline-flex items-center">
+                                        <img
+                                            src="https://cdn-icons-png.flaticon.com/128/763/763812.png"
+                                            alt="Training Icon"
+                                            className="w-6 h-6 mr-3"
+                                        />
+                                        <h3 className="font-bold text-base text-gray-800">{item?.name}</h3>
+                                    </div>
+                                    <p className="text-xs text-gray-500">{item?.created_at}</p>
+                                </div>
+                                <p className="mt-1 text-sm">
+                                    Hey! Do you remember about choosing your training regime?
+                                </p>
+                            </div>
+                        </div>
                     </Dropdown.Item>
-                    <Dropdown.Item variant="divider" />
-                    {ListOfNotification?.data?.slice(0,4).map((item:any,index:any)=>(
-     <Dropdown.Item variant="header">
-     <div className="flex items-center">
-     <div className="mt-2 px-6  bg-white rounded-lg w-full">
-<div className=" inline-flex items-center justify-between w-full">
-<div className="inline-flex items-center">
-<img
-src="https://cdn-icons-png.flaticon.com/128/763/763812.png"
-alt="Training Icon"
-className="w-6 h-6 mr-3"
-/>
-<h3 className="font-bold text-base text-gray-800">{item?.name}</h3>
-</div>
-<p className="text-xs text-gray-500">{item?.created_at}</p>
-</div>
-<p className="mt-1 text-sm">
-Hey! Do you remember about choosing your training regime?
-</p>
-</div>
-     </div>
- </Dropdown.Item>
-                    ))}
-           
+                ))}
+
                 <Dropdown.Item variant="divider" />
                 {dropdownItemList.map((item) => (
                     <Dropdown.Item
@@ -147,7 +147,7 @@ Hey! Do you remember about choosing your training regime?
                     </Dropdown.Item>
                 ))}
                 {/* <Dropdown.Item variant="divider" /> */}
-              {ListOfNotification?.data?.length>0 &&  <Dropdown.Item
+                {ListOfNotification?.data?.length > 0 && <Dropdown.Item
                     eventKey="Sign Out"
                     className="gap-2 text-end"
                     onClick={handleViewAll}
