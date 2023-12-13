1782cb703b91ed68f75baf8632c9c7560354ac24
tanmaya-arora <tanmaya.arora@ikokas.com>
2023-12-13 17:43:55 +0530

    Fixed responsive issue on tablet

--- src/views/auth/Partner/PartnerBussinessTypeCompliances.tsx
+++ src/views/auth/Partner/PartnerBussinessTypeCompliances.tsx
@@ -536,7 +536,7 @@ if(item?.url && (item?.licenseNoVal =='null' || item?.licenseNoVal ==null || !it
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
                             </svg>
                         </span>
-                        <h6 className="font-medium leading-tight">{t("Asset Specifications")} </h6>
+                        <h6 className="font-medium leading-tight pt-1.5">{t("Asset Specifications")} </h6>
                         {/* <p className="text-sm">Step details here</p> */}
                     </li>
                     <li className="mb-10 ml-6">
@@ -545,7 +545,7 @@ if(item?.url && (item?.licenseNoVal =='null' || item?.licenseNoVal ==null || !it
                                 <path d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z" />
                             </svg>
                         </span>
-                        <h6 className="font-medium leading-tight pt-2">{t("Compliance Details")} </h6>
+                        <h6 className="font-medium leading-tight pt-1.5">{t("Compliance Details")} </h6>
                         {/* <p className="text-sm">Step details here</p> */}
                     </li>
 
