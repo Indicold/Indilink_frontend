dd61704cefdfc3812f8c8d183fe164b72c6c0d58
ravindra.kumar <ravindra.kumar@vector3.digital>
2024-02-06 10:35:51 +0530

    upadte store move and prepare details page

--- src/views/auth/Partner/Details/MoveDetails.tsx
+++ src/views/auth/Partner/Details/MoveDetails.tsx
@@ -80,6 +80,78 @@ const MoveDetails = () => {
             setAssetsDetails(fetchDetails?.data?.move)
         }
     }, [fetchDetails?.data?.move])
+    const partoneDetails = [
+        {
+            id: 1,
+            titlePart1: 'Cold',
+            titlePart2: 'Stoarage 1',
+            imageUrl:
+                'https://cdn.pixabay.com/photo/2014/08/08/20/54/laundry-413688_1280.jpg',
+        },
+        {
+            id: 2,
+            titlePart1: 'Cold',
+            titlePart2: 'Stoarage 2',
+            imageUrl:
+                'https://img.freepik.com/premium-photo/caucasian-factory-worker-blue-lab-suit-with-tablet-hands-bend-manufacture-machine-check-readings-look-camera_645730-122.jpg?size=626&ext=jpg',
+        },
+        {
+            id: 3,
+            titlePart1: 'Cold',
+            titlePart2: 'Stoarage 3',
+            imageUrl:
+                'https://img.freepik.com/free-photo/photo-automobile-production-line-welding-car-body-modern-car-assembly-plant-auto-industry_645730-133.jpg',
+        },
+        {
+            id: 4,
+            titlePart1: 'Cold',
+            titlePart2: 'Stoarage 4',
+            imageUrl:
+                'https://img.freepik.com/free-photo/minsk-belarus-dec-15-2021-car-bodies-are-assembly-line-factory-production-cars-modern-automotive-industry-car-being-checked-before-being-painted-hightech-enterprise_645730-813.jpg?t=st=1707128866~exp=1707129466~hmac=d7475cb31502855d6c2cd852626cf926b3e8d1cff81cf09290adb7e0ba15c91f',
+        },
+        {
+            id: 5,
+            titlePart1: 'Cold',
+            titlePart2: 'Stoarage 5',
+            imageUrl:
+                'https://img.freepik.com/premium-photo/industrial-surface_199107-110.jpg?size=626&ext=jpg',
+        },
+        {
+            id: 6,
+            titlePart1: 'Cold',
+            titlePart2: 'Stoarage 6',
+            imageUrl:
+                'https://img.freepik.com/premium-photo/technologist-make-set-up-special-device-while-standing-department-dairy-factory_645730-45.jpg?size=626&ext=jpg',
+        },
+        {
+            id: 7,
+            titlePart1: 'Cold',
+            titlePart2: 'Stoarage 7',
+            imageUrl:
+                'https://img.freepik.com/premium-photo/photo-automobile-production-line-welding-car-body-modern-car-assembly-plant-auto-industry_645730-115.jpg?size=626&ext=jpg',
+        },
+        {
+            id: 8,
+            titlePart1: 'Cold',
+            titlePart2: 'Stoarage 8',
+            imageUrl:
+                'https://img.freepik.com/free-photo/industrial-worker-protective-uniform-operating-forklift-big-warehouse-distribution-center_342744-1497.jpg?size=626&ext=jpg',
+        },
+        {
+            id: 9,
+            titlePart1: 'Cold',
+            titlePart2: 'Stoarage 9',
+            imageUrl:
+                'https://img.freepik.com/premium-photo/one-working-day-modern-automatic-bus-trolleybus-car-production-with-unfinished-cars-workers-protective-uniform-automotive-background-automobile-assembly-line-welding-arm_645730-44.jpg?size=626&ext=jpg',
+        },
+        {
+            id: 10,
+            titlePart1: 'Cold',
+            titlePart2: 'Stoarage 10',
+            imageUrl:
+                'https://img.freepik.com/premium-photo/one-working-day-modern-automatic-bus-trolleybus-car-production-with-unfinished-cars-workers-protective-uniform-factory-background-automobile-assembly-line-welding-arm_645730-74.jpg?size=626&ext=jpg',
+        },
+    ]
     const array = [
         {
             id: 0,
@@ -200,11 +272,11 @@ const MoveDetails = () => {
                         <h2 className="text-gray-400 text-lg">{id}</h2>
                     </b>
                 </h5>
-                <div className="flex gap-4">
-                    <div className="w-[70%]">
+                <div className="lg:flex gap-4">
+                    <div className="lg:w-[70%] mb-4 w-full">
                         <img
-                            className="h-[19rem] w-full rounded-lg"
-                            src="https://flowbite.s3.amazonaws.com/docs/gallery/featured/image.jpg"
+                            className="lg:h-[19rem] w-full rounded-lg"
+                            src="https://img.freepik.com/free-photo/photo-automobile-production-line-welding-car-body-modern-car-assembly-plant-auto-industry-interior-hightech-factory-modern-production_645730-185.jpg?w=826&t=st=1707127221~exp=1707127821~hmac=5e1da4cbe40d01a7461d6e550faf689a1a76a9960d4536c0336fcf1dfb40573chttps://flowbite.s3.amazonaws.com/docs/gallery/featured/image.jpg"
                             alt=""
                         />
                     </div>
@@ -212,28 +284,28 @@ const MoveDetails = () => {
                         <div>
                             <img
                                 className="h-auto lg:h-[9rem] w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+                                src="https://cdn.pixabay.com/photo/2014/09/13/21/46/milling-444493_1280.jpg"
                                 alt=""
                             />
                         </div>
                         <div>
                             <img
                                 className="h-auto lg:h-[9rem] max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+                                src="https://img.freepik.com/free-photo/close-up-shot-screw-cutting-machine-factory-that-makes-metal-details_645730-81.jpg?t=st=1707106617~exp=1707107217~hmac=3d644d054bb6c63a71eed8e12b65d4d77d6b1acc620cb0c1a928d75366a41b1e"
                                 alt=""
                             />
                         </div>
                         <div>
                             <img
                                 className="h-auto lg:h-[9rem] max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+                                src="https://img.freepik.com/free-photo/production-manufacture-sightseeing-excursion-interior-ceramic-tile-manufacturing-plant-ceramic-tile-manufacturing-plant-with-conveyer-belt_645730-195.jpg?t=st=1707106617~exp=1707107217~hmac=07fa02d138ae280c003f2edfe403dd8d33503dc6b01d575b57640b28e346ece8"
                                 alt=""
                             />
                         </div>
                         <div>
                             <img
                                 className="h-auto lg:h-[9rem] max-w-full rounded-lg"
-                                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+                                src="https://img.freepik.com/free-photo/car-bodies-are-assembly-line-factory-production-cars-modern-automotive-industry-car-being-checked-before-being-painted-hightech-enterprise_645730-801.jpg?t=st=1707106617~exp=1707107217~hmac=16ad15258b9de01b1113a937a1b686e36c1fcb89eba0cd48751822361de639f1"
                                 alt=""
                             />
                         </div>
@@ -274,14 +346,14 @@ const MoveDetails = () => {
                             <button
                                 type="button"
                                 onClick={handleAccept}
-                                className="text-white bg-[green]  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                                className="text-white w-full bg-[#103492] hover:bg-[#103492]/80 focus:ring-4 focus:outline-none focus:ring-[#103492]/50 font-medium rounded-lg text-sm px-14 m-auto py-2.5  dark:hover:bg-[#103492]/80 dark:focus:ring-[#103492]/40 "
                             >
                                 Approved
                             </button>
                             <button
                                 type="button"
                                 onClick={handleReject}
-                                className="text-white bg-[red]  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                                className="text-white w-full bg-[#103492] hover:bg-[#103492]/80 focus:ring-4 focus:outline-none focus:ring-[#103492]/50 font-medium rounded-lg text-sm px-14 m-auto py-2.5  dark:hover:bg-[#103492]/80 dark:focus:ring-[#103492]/40 "
                             >
                                 Disapproved
                             </button>
@@ -290,35 +362,43 @@ const MoveDetails = () => {
                         <button
                             type="button"
                             onClick={() => setModalList(true)}
-                            className="w-full text-white bg-[#FF9119] hover:bg-[#FF9119]/80 focus:ring-4 focus:outline-none focus:ring-[#FF9119]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center  dark:hover:bg-[#FF9119]/80 dark:focus:ring-[#FF9119]/40 me-2 mb-2"
+                            className="text-white w-full bg-[#103492] hover:bg-[#103492]/80 focus:ring-4 focus:outline-none focus:ring-[#103492]/50 font-medium rounded-lg text-sm px-14 m-auto py-2.5  dark:hover:bg-[#103492]/80 dark:focus:ring-[#103492]/40 "
                         >
                             View Audit
                         </button>
                     )}
                 </div>
                 <div>
-                    <h1 className="text-lg text-blue-800 my-10">Facilities</h1>
 
-                    <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
-                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map(
-                            (item: any, index: any) => {
+<h1 className='text-lg text-blue-800 my-10'>
+    Facilities
+</h1>
+
+
+<div className="grid grid-cols-2 md:grid-cols-5 gap-4">
+                            {partoneDetails.map((item: any, index: any) => {
                                 return (
                                     <div key={index} className="relative">
                                         <img
                                             className="h-auto max-w-full rounded-lg"
-                                            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
+                                            src={item?.imageUrl}
                                             alt=""
                                         />
-                                        <div className="pl-4 pb-2 absolute font-bold text-white bottom-0 left-0">
-                                            <p>Cold</p>
-                                            <p>Storage</p>
+                                        <div className="pl-4 pb-2 absolute font-bold  text-white bottom-0 left-0">
+                                            <p className="backdrop-blur-[2px]">
+                                                {item?.titlePart1}
+                                            </p>
+
+                                            <p className="backdrop-blur-[2px]">
+                                                {item?.titlePart2}
+                                            </p>
                                         </div>
                                     </div>
                                 )
-                            }
-                        )}
-                    </div>
-                </div>
+                            })}
+                        </div>
+
+</div>
 
                 <div className="ml-4">
                     <h1 className="text-lg text-blue-800 mt-10 mb-4">
